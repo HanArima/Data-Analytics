ef150112e462c1185b4f017d2a2dbbef06bf04a6
HanArima <garhanimasa2003@gmail.com>
2025-03-07 00:21:16 +0530

    K Means Clustering

--- Data Science/Project_1.py
+++ Data Science/Project_1.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score, davies_bouldin_score
 
 df = pd.read_csv("Facebook_Marketplace_data.csv")
 df = df.drop('Column1', axis=1)
@@ -17,11 +20,88 @@ df['time'] = df['status_published'].dt.time
 df['hour_of_day'] = df['status_published'].dt.hour
 
 def main():
-    correlation_matrix()
+    kmeansclustering_scaling()
 
 def display_table(df):
     print(df.head(10))
 
+def kmeansclustering():
+    # Converting the status type from float to Categorical Data
+    df['status_type'] = pd.Categorical(df['status_type']).codes
+    # Selecting features for clustering
+    features = df.drop(['status_published', 'date', 'time', 'year', 'month', 'hour_of_day', 'status_id'], axis=1)
+
+    display_table(features)
+    # Elbow Method
+    wcss = []
+    for i in range(1, 11):
+        kmeans = KMeans(n_clusters=i, init='k-means++', random_state=42)
+        kmeans.fit(features)
+        wcss.append(kmeans.inertia_)
+    
+    plt.figure(figsize=(10,5))
+    sns.lineplot(x=range(1,11), y=wcss, marker='o')
+    plt.title('Elbow Method')
+    plt.xlabel('Number of Clusters')
+    plt.ylabel('WCSS')
+    plt.show()
+
+def kmeansclustering_scaling():
+    # Converting the status type from float to Categorical Data
+    df['status_type'] = pd.Categorical(df['status_type']).codes
+    # Selecting features for clustering
+    features = df.drop(['status_published', 'date', 'time', 'year', 'month', 'hour_of_day', 'status_id'], axis=1)
+    # Scaling the features
+    scal = StandardScaler()
+    scaled_features = scal.fit_transform(features)
+    # Display the first few rows of the scaled features
+    scaled_df = pd.DataFrame(scaled_features, columns=features.columns)
+    display_table(scaled_df)
+
+    ''' Elbow Method '''
+    wcss = []
+    for i in range(1,21):
+        kmean = KMeans(n_clusters=i, init="k-means++", random_state=42)
+        kmean.fit(scaled_df)
+        wcss.append(kmean.inertia_)
+    plt.figure(figsize=(10, 5))
+    sns.lineplot(x=range(1, 21), y=wcss, marker='o')
+    plt.title('Elbow Method')
+    plt.xlabel('Number of Clusters')
+    plt.ylabel('WCSS')
+    plt.show()
+
+    ''' Silhoutte Method '''
+    silhoutte = []
+    for i in range(2,11):
+        kmeans = KMeans(n_clusters=i, init='k-means++', random_state=42)
+        cluster_labels = kmeans.fit_predict(scaled_df)
+
+        silhoutte_avg = silhouette_score(scaled_df, cluster_labels)
+        silhoutte.append(silhoutte_avg)
+    
+    plt.figure(figsize=(10, 5))
+    sns.lineplot(x=range(2,11), y=silhoutte, marker='o')
+    plt.title('Silhouette Method for Optimal k')
+    plt.xlabel('Number of Clusters')
+    plt.ylabel('Silhouette Score')
+    plt.show()
+
+    ''' Davies-Bouldin Index '''
+    dbi = []
+    for i in range(2,11):
+        kmeans = KMeans(n_clusters=i, init='k-means++', random_state=42)
+        cluster_labels = kmeans.fit_predict(scaled_df)
+
+        db_index = davies_bouldin_score(scaled_df, cluster_labels)
+        dbi.append(db_index)
+    
+    plt.figure(figsize=(10, 5))
+    sns.lineplot(x=range(2,11), y=dbi, marker='o')
+    plt.title('Davies-Bouldin Index Method for Optimal k')
+    plt.xlabel('Number of Clusters') 
+    plt.ylabel('Davies-Bouldin Index Score')
+    plt.show()
 
 def correlation_matrix():
     temp_df = df[['num_reactions','num_comments',	'num_shares','num_likes']]
